3e4575c6f1eb77f9f9d892e7c373932c2dbf5e0f
Platonov Dmiry <puena89@gmail.com>
2022-09-27 19:48:31 +0300

    Add task 4

---
+++ Practice_2/task_4.py
@@ -0,0 +1,13 @@
+# Напишите программу которая на вход принимает два числа. Задайте список из N элементов, заполненных числами из промежутка [-N, N]
+
+nubmer_of_elements = int(input("Введите количество элементов. "))
+list_of_elements = [e for e in range(-nubmer_of_elements, nubmer_of_elements + 1)]
+print(list_of_elements)
+print("Введите индексы элементов которые необходимо перемножить!")
+first_position = int(input("Введите первый индекс "))
+second_postition = int(input("Введите второй индекс "))
+if (first_position >= 0 and first_position < len(list_of_elements)
+    and second_postition >= 0 and second_postition < len(list_of_elements)):
+    print("Произведение равно: ", list_of_elements[first_position] * list_of_elements[second_postition])
+else:
+    print("Недопустимый индекс!")
