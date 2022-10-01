f66f700037c0f7f996257241c5694691a612242a
Platonov Dmiry <puena89@gmail.com>
2022-10-01 22:07:37 +0300

    Add task 1,2

--- Practice_2/task_5.py
+++ Practice_2/task_5.py
@@ -6,6 +6,7 @@ def my_shuffle(list):
     for i in reversed(range(len(list))):
         j = random.randint(0, i)
         list[i], list[j] = list[j], list[i]
+        
 
 
 list_size = int(input("Введите размер списка: "))
@@ -13,4 +14,3 @@ created_list = [random.randrange(-(list_size*10), (list_size+1)*10, 1) for e in 
 print(created_list)
 my_shuffle(created_list)
 print(created_list)
-
---
+++ Practice_3/task_2.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+def my_func(list):
+    my_list = []
+    _array_lenght = int(len(list) / 2 + len(list) % 2)
+    for i in range(_array_lenght):
+        my_list.append(list[i]*list[len(list)-1-i])
+    return my_list
+
+print(my_func([2, 3, 4, 5, 6]))
