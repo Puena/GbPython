1b890f30febfec4c461537ed017ab5be16ef57f7
Platonov Dmiry <puena89@gmail.com>
2022-10-01 11:18:41 +0300

    Fix task 4 description

--- Practice_2/task_4.py
+++ Practice_2/task_4.py
@@ -4,8 +4,8 @@ nubmer_of_elements = int(input("Введите количество элемен
 list_of_elements = [e for e in range(-nubmer_of_elements, nubmer_of_elements + 1)]
 print(list_of_elements)
 print("Введите индексы элементов которые необходимо перемножить!")
-first_position = int(input("Введите первый индекс ")) - 1
-second_postition = int(input("Введите второй индекс ")) - 1
+first_position = int(input("Введите первую позицию ")) - 1
+second_postition = int(input("Введите вторая позицию ")) - 1
 if (first_position >= 0 and first_position < len(list_of_elements)
     and second_postition >= 0 and second_postition < len(list_of_elements)):
     print("Произведение равно: ", list_of_elements[first_position] * list_of_elements[second_postition])
