e347704eb6f77f98b826ea3f827e185aaca6bb2c
Platonov Dmiry <puena89@gmail.com>
2022-10-06 22:25:07 +0300

    Add Practice 4 task_2

---
+++ Practice_4/task_2.py
@@ -0,0 +1,24 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+
+
+import math
+
+
+def primefactors(n):
+    while n % 2 == 0:
+        print (2),
+        n = int(n / 2)
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while (n % i == 0):
+            print (i)
+            n = int(n / i)
+
+    if n == 1:
+        print(n)
+    elif n > 2:
+        print (n)
+ 
+natural_number = int(input("Enter number: "))
+primefactors(natural_number)
