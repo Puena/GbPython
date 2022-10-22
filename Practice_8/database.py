aeb86be56baeab857a36a2277555eabb63fb6d26
Dmitry Platonov <woweuropeluffy@gmail.com>
2022-10-22 09:21:36 +0300

    Init proj

--- Practice_8/database.py
+++ Practice_8/database.py
@@ -2,11 +2,62 @@ import sqlite3
 from sqlite3 import Error
 
 
-def connect(db_name: str):
+DB_NAME = "school.db"
+
+
+def connect():
     try:
-        con = sqlite3.connect(db_name)
-        print(f'Connected to database {db_name}')
+        con = sqlite3.connect(DB_NAME)
+        print(f'Connected to database {DB_NAME}')
         return con
     except Error:
         print(Error)
         raise Error
+
+
+conn = connect()
+cursor = conn.cursor()
+studentsTable = """CREATE TABLE IF NOT EXISTS students(
+        id int primary key,
+        name text not null,
+        lastname text not null,
+        birthdate date not null
+        )"""
+conn.execute(studentsTable)
+classRoomsTable = """CREATE TABLE IF NOT EXISTS classrooms(
+        number int primary key
+        )"""
+conn.execute(classRoomsTable)
+classesTable = """CREATE TABLE IF NOT EXISTS classes(
+        id int primary key,
+        title text not null
+        )"""
+conn.execute(classesTable)
+placesTable = """CREATE TABLE IF NOT EXISTS places(
+        id primary key,
+        row int not null,
+        cell int not null,
+        variant int not null,
+        classroom_number int not null,
+        FOREIGN KEY(classroom_number) REFERENCES classrooms(number)
+        )"""
+conn.execute(placesTable)
+classAndStudentsTable = """CREATE TABLE IF NOT EXISTS class_students(
+        class_number int not null,
+        student_id int not null,
+        FOREIGN KEY(class_number) REFERENCES classes(number),
+        FOREIGN KEY(student_id) REFERENCES students(id)
+        )"""
+conn.execute(classAndStudentsTable)
+studentsOnPlaceTable = """CREATE TABLE IF NOT EXISTS students_on_place(
+        id int primary key,
+        student_id int not null,
+        place_id int not null,
+        took_at timestamp not null,
+        free_at timestamp,
+        FOREIGN KEY(student_id) REFERENCES students(id),
+        FOREIGN KEY(place_id) REFERENCES places(id)
+        )"""
+conn.execute(studentsOnPlaceTable)
+conn.commit()
+conn.close()
---
+++ Practice_8/students.py
@@ -0,0 +1,57 @@
+import database
+
+
+def find(id: int):
+    conn = database.connect()
+    cursor = conn.cursor()
+    query = """select * from students where id = ?"""
+    cursor.execute(query, (id))
+    records = cursor.fetchall()
+    return {
+        "id": records[0][0],
+        "name": records[0][1],
+        "lastname": records[0][2],
+        "birthdate": records[0][3]
+    }
+
+
+def findAll():
+    conn = database.connect()
+    cursor = conn.cursor()
+    query = """select * from students where id = ?"""
+    cursor.execute(query, (id))
+    records = cursor.fetchall()
+    return map(lambda r:
+               {"id": r[0],
+                "name": r[1],
+                "lastname": r[2],
+                "birthdate": r[3]}, records)
+
+
+def insert(name, lastname, birthdate):
+    conn = database.connect()
+    cursor = conn.cursor()
+    query = """INSERT INTO students (name, lastname, birthdate)
+                            VALUES (?, ?, ?)"""
+    cursor.execute(query, (name, lastname, birthdate))
+    conn.commit()
+    cursor.close()
+
+
+def update(id: int, name: str, lastname: str, birthdate):
+    conn = database.connect()
+    cursor = conn.cursor()
+    query = """UPDATE students set name = ?, lastname = ?, birthdate = ?
+                                where id = ?"""
+    cursor.execute(query, (name, lastname, birthdate, id))
+    conn.commit()
+    conn.close()
+
+
+def delete(id: int):
+    conn = database.connect()
+    cursor = conn.cursor()
+    query = """DELETE students where id = ?"""
+    cursor.execute(query, (id))
+    conn.commit()
+    conn.close()
